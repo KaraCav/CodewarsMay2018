52396c7ce85b3d0c3553167a9f2a7ceaea4d2113
Kara Cavanaugh <karacav@msn.com>
2018-05-04 23:39:53 -0400

    Fixed Solution 1

--- scripts.js
+++ scripts.js
@@ -1,26 +1,22 @@
 // NUMBER 1: You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
-function practiceFunction(arr) {
+function findEvenIndex(arr) {
     for (i = 1; i < arr.length; i++) {
+        console.log('overall ', i);
         totalOne = 0;
         totalTwo = 0;
         var arr1 = [];
         var arr2 = [];
         for (j = 0; j < i; j++) {
             arr1.push(arr[j]);
-            totalOne = totalOne + j;
-            console.log('arr 1 ', arr1, totalOne);
+            totalOne = totalOne + arr[j];
         }
-        for (k = i; k < arr.length; k++) {
+        for (k = i + 1; k < arr.length; k++) {
             arr2.push(arr[k]);
-            totalTwo = totalTwo + k;
-            console.log('arr 2 ', arr2, totalTwo);
-        }
-        if (totalOne == totalTwo) {
-            console.log('here is i ', i);
-            return i;
-        }
-        else {
-            console.log('broken');
+            totalTwo = totalTwo + arr[k];
+            if (totalOne == totalTwo) {
+                return i;
+            }
         }
     }
+    return -1;
 }
