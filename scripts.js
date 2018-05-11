8806f1257e58982d9f2d40f77f891ca975f2fc25
Kara Cavanaugh <karacav@msn.com>
2018-05-10 21:11:36 -0400

    Solution 3A

--- scripts.js
+++ scripts.js
@@ -71,3 +71,12 @@ function disariumNumber(n) {
         return "Not !!";
     }
 }
+//OR can do:
+function disariumNumber(n) {
+    return Number(String(n).split``
+        .map((x, i) => Math
+            .pow(x, i + 1))
+        .reduce((a, b) => a + b, 0)) === n
+        ? "Disarium !!"
+        : "Not !!"
+}
