c300655bfc1d3303b6f48027bc42658ff9b1bf7b
Kara Cavanaugh <karacav@msn.com>
2018-05-14 18:11:29 -0400

    Solution 6

--- scripts.js
+++ scripts.js
@@ -104,3 +104,27 @@ function minValue(values) {
     }
     return Number(numArr.join(''));
 }
+
+// NUMBER 6: Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on. Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+function rowWeights(array) {
+    array1 = [0];
+    array2 = [0];
+    for (let i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            array1.push(array[i]);
+        }
+        else {
+            array2.push(array[i]);
+        }
+    }
+    let finalArr = [];
+    let finalNum1 = array1.reduce(function (total, num) {
+        return total + num;
+    });
+    let finalNum2 = array2.reduce(function (total, num) {
+        return total + num;
+    });
+    finalArr.push(finalNum1);
+    finalArr.push(finalNum2);
+    return finalArr;
+}
